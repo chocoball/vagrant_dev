f4419646b9b0e5acb082b9bdb9e928ef420e710c
chocoball <shinji.sadato@gmail.com>
2015-04-22 14:10:17 +0900

    provision
    pyenv, rbenv 追加

--- provision.sh
+++ provision.sh
@@ -8,8 +8,7 @@ home=/home/vagrant
 # install apt package
 ###############################################################################
 apt-get update -y
-apt-get install -y git tmux zsh htop vim
-
+apt-get install -y git tmux zsh htop vim gcc make openssl libssl-dev libbz2-dev libreadline-dev libsqlite3-dev libffi-dev
 
 ###############################################################################
 # cloning prezto
@@ -57,6 +56,45 @@ if [ ! -e ${home}/zpreztorc_modified ]; then
     sed -i -e "s/nano/vim/g" ${home}/.zprofile
     touch ${home}/zpreztorc_modified
 fi
+
+###############################################################################
+# install Python development environment
+###############################################################################
+if [ ! -d ${home}/.pyenv ]; then
+    git clone https://github.com/yyuu/pyenv.git ${home}/.pyenv
+    mkdir -p ${home}/.pyenv/versions ${home}/.pyenv/shims
+
+    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ${home}/.bashrc
+    echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ${home}/.bashrc
+    echo 'eval "$(pyenv init -)"' >> ${home}/.bashrc
+
+    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ${home}/.zshenv
+    echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ${home}/.zshenv
+    echo 'eval "$(pyenv init -)"' >> ${home}/.zshenv
+
+
+    git clone https://github.com/yyuu/pyenv-virtualenv.git ${home}/.pyenv/plugins/pyenv-virtualenv
+
+    echo 'eval "$(pyenv virtualenv-init -)"' >> ${home}/.bashrc
+    echo 'eval "$(pyenv virtualenv-init -)"' >> ${home}/.zshenv
+fi
+
+###############################################################################
+# install Ruby development environment
+###############################################################################
+if [ ! -d ${home}/.rbenv ]; then
+    git clone https://github.com/sstephenson/rbenv.git ${home}/.rbenv
+
+    echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ${home}/.bashrc
+    echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ${home}/.zshenv
+
+    echo 'eval "$(rbenv init -)"' >> ${home}/.bashrc
+    echo 'eval "$(rbenv init -)"' >> ${home}/.zshenv
+
+
+    git clone https://github.com/sstephenson/ruby-build.git ${home}/.rbenv/plugins/ruby-build
+fi
+
 
 
 ###############################################################################
